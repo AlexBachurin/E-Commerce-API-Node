08cdc586f0b92a3117100b4d5efb83924da17c2c
Aleksandr <alexbachurin91@gmail.com>
2023-10-11 21:26:26 +0300

    different approaches in adding user role functionality

--- controllers/authControllers.js
+++ controllers/authControllers.js
@@ -3,14 +3,20 @@ const { StatusCodes } = require("http-status-codes");
 const { BadRequestError } = require("../errors");
 
 const register = async (req, res) => {
-  const { email } = req.body;
+  const { email, name, password } = req.body;
   //check for duplicate email, try to find this email in db
   const emailAlreadyInUse = await UserModel.findOne({ email });
   //   if user exists throw error, if not create new user
   if (emailAlreadyInUse) {
     throw new BadRequestError("Email already in use");
   }
-  const user = await UserModel.create(req.body);
+  //   first registered user is an admin functionality
+  //const isFirstAccount = await User.countDocuments({}) === 0;
+  // const role = isFirstAccount ? 'admin' : 'user';
+
+  //for more security on user creation: destructure and pass only
+  //name, email, pw etc. without user role,which can be admin
+  const user = await UserModel.create({ name, email, password, role });
   res.status(StatusCodes.CREATED).json({ user });
 };
 
