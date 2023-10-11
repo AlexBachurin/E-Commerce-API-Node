d5702c06f76891c377f870cf4eabe053d81f48ec
Aleksandr <alexbachurin91@gmail.com>
2023-10-11 19:58:53 +0300

    added validator package for email validation in UserSchema

--- models/User-Model.js
+++ models/User-Model.js
@@ -1,4 +1,5 @@
 const mongoose = require("mongoose");
+const validator = require("validator");
 
 const UserSchema = new mongoose.Schema({
   name: {
@@ -12,10 +13,14 @@ const UserSchema = new mongoose.Schema({
     required: [true, "Please provide email"],
     // use regex for validation or package
     // regex for checking email
-    match: [
-      /^(([^<>()[\]\\.,;:\s@"]+(\.[^<>()[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/,
-      "Please provide valid email",
-    ],
+    // match: [
+    //   /^(([^<>()[\]\\.,;:\s@"]+(\.[^<>()[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/,
+    //   "Please provide valid email",
+    // ],
+    validate: {
+      validator: validator.isEmail,
+      message: "Please provide valid email",
+    },
     //unique index, if email in use we get duplicate message
     unique: true,
   },
