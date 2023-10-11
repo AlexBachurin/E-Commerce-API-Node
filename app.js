7efce01f78627a157c7dff577182afb002152ca9
Aleksandr <alexbachurin91@gmail.com>
2023-10-11 19:54:30 +0300

    Project setup + connect to DB + Basic routers and middleware setup + User Model

---
+++ app.js
@@ -0,0 +1,45 @@
+// *** IMPORTS ***
+const express = require("express");
+// this middleware will apply try catch block in our controllers
+// automatically so we dont need to add it manually
+require("express-async-errors");
+const connectDB = require("./db/connect");
+require("dotenv").config();
+const errorHandler = require("./middleware/error-handler");
+const notFound = require("./middleware/not-found");
+const morgan = require("morgan");
+
+const app = express();
+
+// *** MIDDLEWARE ***
+// morgan
+app.use(morgan("tiny"));
+// json middleware
+app.use(express.json());
+
+// *** ROUTES ***
+app.get("/", (req, res) => {
+  res.send("<h1>E-commerce App</h1>");
+});
+//errors middleware    404(not found)goes first!!!
+// we hit this only then route does not exist
+app.use(notFound);
+// this for handling errors in existing routes
+app.use(errorHandler);
+
+// *** CONNECT TO SERVER ***
+const port = process.env.PORT || 5000;
+
+const connect = async () => {
+  try {
+    //connect to db
+    await connectDB(process.env.MONGO_URL);
+    app.listen(port, () => {
+      console.log(`Server is listening on port: ${port}`);
+    });
+  } catch (error) {
+    console.log(error);
+  }
+};
+
+connect();
