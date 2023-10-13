8420d8054773c8c6c23678baf555f2b103800b31
Aleksandr <alexbachurin91@gmail.com>
2023-10-13 20:06:01 +0300

    getAllUsers and getSingleUser controller functionality added

--- controllers/userController.js
+++ controllers/userController.js
@@ -1,9 +1,21 @@
-const getAllUsers = (req, res) => {
-  res.send("Get All Users");
+const { BadRequestError, NotFoundError } = require("../errors");
+const UserModel = require("../models/User-Model");
+const { StatusCodes } = require("http-status-codes");
+
+const getAllUsers = async (req, res) => {
+  //find all users whom role is "user" and remove password from response
+  const users = await UserModel.find({ role: "user" }).select("-password");
+
+  res.status(StatusCodes.OK).json({ users });
 };
 
-const getSingleUser = (req, res) => {
-  res.send("Get Single User");
+const getSingleUser = async (req, res) => {
+  const { id } = req.params;
+  const user = await UserModel.findOne({ _id: id }).select("-password");
+  if (!user) {
+    throw new NotFoundError("No User with such id");
+  }
+  res.status(StatusCodes.OK).json({ user });
 };
 
 const showCurrentUser = (req, res) => {
