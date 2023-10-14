54107b5ff6c5e918e15b588b34cfbdb7ccdf90d7
Aleksandr <alexbachurin91@gmail.com>
2023-10-14 23:04:13 +0300

    Create Product functionality

--- controllers/productController.js
+++ controllers/productController.js
@@ -1,9 +1,17 @@
+const ProductModel = require("../models/ProductModel");
+const { StatusCodes } = require("http-status-codes");
+
 const getAllProducts = (req, res) => {
   res.send("get all products");
 };
 
-const createProduct = (req, res) => {
-  res.send("create product");
+const createProduct = async (req, res) => {
+  //attach user to req.body, since we have user property in Product Model
+  //we get it from req.user from authentication middleware
+  req.body.user = req.user.userId;
+  //create product and send it back
+  const product = await ProductModel.create(req.body);
+  res.status(StatusCodes.CREATED).json({ product });
 };
 
 const getSingleProduct = (req, res) => {
